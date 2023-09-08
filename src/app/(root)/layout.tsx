7e5d041567ee37f0b842b0e8ff5b27cbd86e05de
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2023-09-08 15:31:59 +0200

    Switch font to Montserrat

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,16 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-import { Topbar } from "@/components/layout";
+import { Sidebar, Topbar } from "@/components/layout";
 
 import "../globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Voltwise",
@@ -22,9 +25,12 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={montserrat.className}>
           <Topbar />
-          <main className="bg-black w-full h-[100vh] pt-16">{children}</main>
+          <main className="bg-black w-full h-[100vh] pt-16 flex">
+            <Sidebar />
+            {children}
+          </main>
         </body>
       </html>
     </ClerkProvider>
