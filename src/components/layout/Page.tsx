4d61e8a8b8c6c130e75c48a116d39317910cc32f
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2023-09-09 20:30:00 +0200

    Update margin in Page component

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -8,7 +8,7 @@ type PageProps = PropsWithChildren<{
 export const Page: React.FC<PageProps> = ({ header, headerNote, children }) => {
   return (
     <div className="w-full flex flex-col overflow-y-scroll m-10 flex-1">
-      <div className="flex items-center gap-3 mb-20">
+      <div className="flex items-center gap-3 mb-12">
         <h1 className="text-2xl">{header}</h1>
         {headerNote && (
           <>
