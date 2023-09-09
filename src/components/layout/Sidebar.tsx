9cc4b681110a84d83c7eedec5088604628ba2478
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2023-09-09 16:22:04 +0200

    Add Settings and Home links to Sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,12 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Home, Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Settings } from "lucide-react";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <section className="w-56 border-r border-[#ffffff22] overflow-y-scroll p-4 flex flex-col items-center justify-end">
-      <Button variant="ghost">
-        <Settings className="mr-2" size={20} /> Ρυθμίσεις
-      </Button>
+    <section className="w-56 border-r border-[#ffffff22] overflow-y-scroll p-4 flex flex-col items-center justify-between">
+      <Link href="/">
+        <Button
+          variant="ghost"
+          className={`w-40 flex justify-start ${
+            pathname === "/" ? "bg-neutral-800" : ""
+          }`}
+        >
+          <Home className="mr-4" size={20} /> Αρχική
+        </Button>
+      </Link>
+      <Link href="/settings">
+        <Button
+          variant="ghost"
+          className={`w-40 flex justify-start ${
+            pathname === "/settings" ? "bg-neutral-800" : ""
+          }`}
+        >
+          <Settings className="mr-4" size={20} /> Ρυθμίσεις
+        </Button>
+      </Link>
     </section>
   );
 };
