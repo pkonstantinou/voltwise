a274f7796e3fa1e2170ca1e4e955ffc354d60e91
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2023-09-15 22:34:38 +0200

    Add readings item on the sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { Home, Settings } from "lucide-react";
+import { Home, Gauge, Settings } from "lucide-react";
 import { Button } from "@/components/buttons";
 
 export const Sidebar = () => {
@@ -11,16 +11,28 @@ export const Sidebar = () => {
 
   return (
     <section className="w-56 border-r border-neutral-800 overflow-y-auto p-4 flex flex-col items-center justify-between">
-      <Link href="/">
-        <Button
-          variant="ghost"
-          className={`w-40 flex justify-start ${
-            pathname === "/" ? "bg-neutral-800" : ""
-          }`}
-        >
-          <Home className="mr-4" size={20} /> Αρχική
-        </Button>
-      </Link>
+      <div className="flex flex-col gap-1.5">
+        <Link href="/">
+          <Button
+            variant="ghost"
+            className={`w-40 flex justify-start ${
+              pathname === "/" ? "bg-neutral-800" : ""
+            }`}
+          >
+            <Home className="mr-4" size={20} /> Αρχική
+          </Button>
+        </Link>
+        <Link href="/readings">
+          <Button
+            variant="ghost"
+            className={`w-40 flex justify-start ${
+              pathname.includes("/readings") ? "bg-neutral-800" : ""
+            }`}
+          >
+            <Gauge className="mr-4" size={20} /> Μετρήσεις
+          </Button>
+        </Link>
+      </div>
       <Link href="/settings">
         <Button
           variant="ghost"
