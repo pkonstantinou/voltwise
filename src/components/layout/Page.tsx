feb97ad962dbec4708b66b3043740f58651a2ae0
Paschalis Konstantinou <pasc.konstantinou@gmail.com>
2023-09-16 16:20:51 +0200

    Modify Page component to accept widget and not headerNote

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -2,20 +2,18 @@ import { PropsWithChildren } from "react";
 
 type PageProps = PropsWithChildren<{
   header: string;
-  headerNote?: string;
+  widget?: React.ReactNode;
 }>;
 
-export const Page: React.FC<PageProps> = ({ header, headerNote, children }) => {
+export const Page: React.FC<PageProps> = ({ header, widget, children }) => {
   return (
     <div className="w-full flex flex-col overflow-y-auto m-10 flex-1">
-      <div className="flex items-center gap-3 mb-12">
-        <h1 className="text-2xl">{header}</h1>
-        {headerNote && (
-          <>
-            <div className="w-0.5 bg-orange-500 h-6" />
-            <p className="text-neutral-400">{headerNote}</p>
-          </>
-        )}
+      <div className="flex mb-12 gap-4 items-start">
+        <div className="flex gap-4 items-end">
+          <h1 className="text-2xl">{header}</h1>
+          {!!widget && <div className="w-0.5 bg-neutral-500 h-6" />}
+        </div>
+        {!!widget && widget}
       </div>
       {children}
     </div>
